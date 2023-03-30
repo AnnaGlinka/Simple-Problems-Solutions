8c7621e5a1d3344522356e2f22a3439274f6ce71
AnnaGlinka <aglinka8@gmail.com>
2023-03-30 18:59:59 +0000

    Reverse bits solutions

--- Reverse_bits_2.py
+++ Reverse_bits_2.py
@@ -1,18 +1,21 @@
 """
 https://leetcode.com/problems/reverse-bits/
 https://www.youtube.com/watch?v=vy288ZcbWRc&t=269s
+https://realpython.com/python-bitwise-operators/
 """
 
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
         for i in range(32):
-            result <<= 1
+            result <<= 1 #x2
             result = result | (n & 1)
             n >>= 1
 
         return result
-    
+
+s = Solution()
+print(s.reverseBits(0xffffffff00000000))
         
 
 
--- Reverse_bits_3.py
+++ Reverse_bits_3.py
@@ -1,7 +1,10 @@
 """
 https://leetcode.com/problems/reverse-bits/
 https://www.youtube.com/watch?v=ZW7st_pN05w
-
+https://towardsdatascience.com/understanding-bitmask-for-the-coding-interview-b1643f4b0e24
+Turn the i^th bit to 1: mask = mask | (1 << i) , or mask |= (1 << i)
+Flip the i^th bit: mask = mask ^ (1 << i) , or mask ^= (1 << i)
+https://www.rapidtables.com/convert/number/hex-to-decimal.html
 """
 
 class Solution:
@@ -13,7 +16,18 @@ class Solution:
         n = (n & 0xaaaaaaaa) >> 1 | (n & 0x55555555) << 1
         
         return n
-    
+
+
+# 0xffff0000 -> 11111111111111110000000000000000
+# 0x0000ffff -> 1111111111111111
+# 0xff00ff00 -> 11111111000000001111111100000000
+# 0x00ff00ff -> 111111110000000011111111
+# 0xf0f0f0f0 -> 11110000111100001111000011110000
+# 0x0f0f0f0f -> 1111000011110000111100001111
+# 0xcccccccc -> 11001100110011001100110011001100
+# 0x33333333 -> 110011001100110011001100110011
+# 0xaaaaaaaa -> 10101010101010101010101010101010
+# 0x55555555 -> 1010101010101010101010101010101
         
 
 
